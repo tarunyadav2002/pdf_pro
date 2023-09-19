93d8e93d8379c42e211d30feb001fbd9e8e33e01
tarunyadav2002 <121471688+tarunyadav2002@users.noreply.github.com>
2023-09-19 23:53:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="p", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 for index,row in df.iterrows():
@@ -10,5 +11,17 @@ for index,row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10,21,200,21)
+
+    pdf.ln(265)
+
+    pdf.set_font(family="Times", style="I", size=10)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
+        pdf.ln(265)
+
+        pdf.set_font(family="Times", style="I", size=10)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
